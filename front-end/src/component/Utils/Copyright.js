17dadaee57e0391e988a1c3df9d3694d88ac94e1
Nhu Nguyen <nhunguyen1259@gmail.com>
2020-04-16 09:14:48 +0700

    copyright fix

--- front-end/src/component/Utils/Copyright.js
+++ front-end/src/component/Utils/Copyright.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import {Link, Typography} from '@material-ui/core';
+import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
 
 function Copyright() {
     return (
         <Typography variant="body2" color="textSecondary" align="center">
           {'Copyright © '}
           <Link color="inherit" href="https://material-ui.com/">
-            RelaMa Page
+            RelaMa Page <FavoriteBorderIcon />
           </Link>{' '}
           {new Date().getFullYear()}
           {'.'}
         </Typography>
     );
-}    
+} 
 
 export default Copyright;
